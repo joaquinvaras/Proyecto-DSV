aa1676adb35632a39d34291421609573d9a275cb
Dioegox <degalvez@miuandes.cl>
2025-05-13 18:17:52 -0400

    fix alumno_id reference

--- Service/import_service.py
+++ Service/import_service.py
@@ -4,28 +4,26 @@ from db import DatabaseConnection
 class ImportService:
     def __init__(self):
         self.db = DatabaseConnection()
-    
+
+    def _success(self, message):
+        print(f"[SUCCESS] {message}")
+
+    def _error(self, message):
+        print(f"[ERROR] {message}")
+
     def import_json(self, file, file_type):
         data = json.load(file)
 
-        if file_type == 'alumnos':
-            self._import_alumnos(data)
-        elif file_type == 'profesores':
-            self._import_profesores(data)
-        elif file_type == 'cursos':
-            self._import_cursos(data)
-        elif file_type == 'instancias_cursos':
-            self._import_instancias_cursos(data)
-        elif file_type == 'instancias_cursos_secciones':
-            self._import_instancias_cursos_secciones(data)
-        elif file_type == 'alumnos_seccion':
-            self._import_alumnos_seccion(data)
-        elif file_type == 'notas_alumnos':
-            self._import_notas_alumnos(data)
-        elif file_type == 'salas_clases':
-            self._import_salas_clases(data)
-        else:
-            raise ValueError(f"Tipo de archivo no soportado: {file_type}")
+        match file_type:
+            case 'alumnos': self._import_alumnos(data)
+            case 'profesores': self._import_profesores(data)
+            case 'cursos': self._import_cursos(data)
+            case 'instancias_cursos': self._import_instancias_cursos(data)
+            case 'instancias_cursos_secciones': self._import_instancias_cursos_secciones(data)
+            case 'alumnos_seccion': self._import_alumnos_seccion(data)
+            case 'notas_alumnos': self._import_notas_alumnos(data)
+            case 'salas_clases': self._import_salas_clases(data)
+            case _: raise ValueError(f"Tipo de archivo no soportado: {file_type}")
 
     def _import_alumnos(self, data):
         cursor = self.db.connect()
@@ -44,9 +42,9 @@ class ImportService:
 
             try:
                 cursor.execute(query, values)
-                print(f"Imported alumno: {name} ({email})")
+                self._success(f"Imported alumno: {name} ({email})")
             except Exception as err:
-                print(f"Error inserting alumno {name} ({email}): {err}")
+                self._error(f"Inserting alumno {name} ({email}): {err}")
 
         self.db.commit()
 
@@ -67,9 +65,9 @@ class ImportService:
 
             try:
                 cursor.execute(query, values)
-                print(f"Imported profesor: {name} ({email})")
+                self._success(f"Imported profesor: {name} ({email})")
             except Exception as err:
-                print(f"Error inserting profesor {name} ({email}): {err}")
+                self._error(f"Inserting profesor {name} ({email}): {err}")
 
         self.db.commit()
 
@@ -88,9 +86,9 @@ class ImportService:
                     INSERT INTO Courses (id, nrc, name, credits)
                     VALUES (%s, %s, %s, %s)
                 """, (curso_id, codigo, descripcion, creditos))
-                print(f"Inserted course: {codigo} - {descripcion}")
+                self._success(f"Inserted course: {codigo} - {descripcion}")
             except Exception as e:
-                print(f"Error inserting course {codigo}: {e}")
+                self._error(f"Inserting course {codigo}: {e}")
         
         for curso in cursos:
             curso_id = curso['id']
@@ -106,13 +104,13 @@ class ImportService:
                             INSERT INTO CoursePrerequisites (course_id, prerequisite_id)
                             VALUES (%s, %s)
                         """, (curso_id, prereq_id))
-                        print(f"Added prerequisite {cod_requisito} for course {curso_id}")
+                        self._success(f"Added prerequisite {cod_requisito} for course {curso_id}")
                     else:
-                        print(f"Prerequisite course with code {cod_requisito} not found")
+                        self._error(f"Prerequisite course with code {cod_requisito} not found")
                 except Exception as e:
-                    print(f"Error adding prerequisite {cod_requisito} for course {curso_id}: {e}")
-        self.db.commit()
+                    self._error(f"Adding prerequisite {cod_requisito} for course {curso_id}: {e}")
 
+        self.db.commit()
 
     def _import_instancias_cursos(self, data):
         cursor = self.db.connect()
@@ -131,10 +129,10 @@ class ImportService:
                     INSERT INTO Instances (id, period, course_id)
                     VALUES (%s, %s, %s)
                 """, (instancia_id, period, curso_id))
-                print(f"Inserted instance {instancia_id} for course {curso_id} in period {period}")
+                self._success(f"Inserted instance {instancia_id} for course {curso_id} in period {period}")
             except Exception as e:
-                print(f"Error inserting instance {instancia_id}: {e}")
-        
+                self._error(f"Inserting instance {instancia_id}: {e}")
+
         self.db.commit()
 
     def _import_instancias_cursos_secciones(self, data):
@@ -155,11 +153,11 @@ class ImportService:
                 """, (profesor_import_id,))
                 result = cursor.fetchone()
                 if not result:
-                    print(f"[ERROR] No professor found with import_id {profesor_import_id}")
+                    self._error(f"No professor found with import_id {profesor_import_id}")
                     continue
                 profesor_id = result["id"]
             except Exception as e:
-                print(f"[ERROR] Fetching professor ID for import_id {profesor_import_id}: {e}")
+                self._error(f"Fetching professor ID for import_id {profesor_import_id}: {e}")
                 continue
 
             try:
@@ -169,19 +167,19 @@ class ImportService:
                 current_count = cursor.fetchone()['COUNT(*)']
                 number = current_count + 1
             except Exception as e:
-                print(f"[ERROR] Counting existing sections for instance_id {instancia_id}: {e}")
+                self._error(f"Counting sections for instance_id {instancia_id}: {e}")
                 continue
 
-            weight_or_percentage = True if tipo_evaluacion == "peso" else False
+            weight_or_percentage = tipo_evaluacion == "peso"
 
             try:
                 cursor.execute("""
                     INSERT INTO Sections (id, instance_id, number, professor_id, weight_or_percentage)
                     VALUES (%s, %s, %s, %s, %s)
                 """, (seccion_id, instancia_id, number, profesor_id, weight_or_percentage))
-                print(f"[SUCCESS] Inserted Section ID {seccion_id}")
+                self._success(f"Inserted Section ID {seccion_id}")
             except Exception as e:
-                print(f"[ERROR] Inserting Section ID {seccion_id}: {e}")
+                self._error(f"Inserting Section ID {seccion_id}: {e}")
                 continue
 
             for topic in combinacion_topicos:
@@ -191,22 +189,19 @@ class ImportService:
 
                 topic_eval = topicos_dict[str(topic_id)]
                 topic_eval_tipo = topic_eval["tipo"]
-                topic_weight_or_percentage = True if topic_eval_tipo == "peso" else False
+                topic_weight_or_percentage = topic_eval_tipo == "peso"
 
                 try:
                     cursor.execute("""
                         INSERT INTO Topics (id, section_id, name, weight, weight_or_percentage)
                         VALUES (%s, %s, %s, %s, %s)
                     """, (topic_id, seccion_id, topic_name, topic_valor, topic_weight_or_percentage))
-                    print(f"[SUCCESS] Inserted Topic ID {topic_id} for Section ID {seccion_id}")
+                    self._success(f"Inserted Topic ID {topic_id} for Section ID {seccion_id}")
                 except Exception as e:
-                    print(f"[ERROR] Inserting Topic ID {topic_id} in Section ID {seccion_id}: {e}")
+                    self._error(f"Inserting Topic ID {topic_id} in Section ID {seccion_id}: {e}")
                     continue
 
-                valores = topic_eval["valores"]
-                obligatorias = topic_eval["obligatorias"]
-
-                for i, (valor, obligatorio) in enumerate(zip(valores, obligatorias)):
+                for i, (valor, obligatorio) in enumerate(zip(topic_eval["valores"], topic_eval["obligatorias"])):
                     activity_weight = valor * 10
                     optional_flag = not obligatorio
 
@@ -215,23 +210,32 @@ class ImportService:
                             INSERT INTO Activities (topic_id, weight, optional_flag, instance)
                             VALUES (%s, %s, %s, %s)
                         """, (topic_id, activity_weight, optional_flag, i+1))
-                        print(f"[SUCCESS] Inserted Activity {i} for Topic ID {topic_id}")
+                        self._success(f"Inserted Activity {i} for Topic ID {topic_id}")
                     except Exception as e:
-                        print(f"[ERROR] Inserting Activity {i} for Topic ID {topic_id}: {e}")
-                        
+                        self._error(f"Inserting Activity {i} for Topic ID {topic_id}: {e}")
+
         self.db.commit()
 
-
     def _import_alumnos_seccion(self, data):
-        
         cursor = self.db.connect()
         alumnos_seccion = data["alumnos_seccion"]
 
         for entry in alumnos_seccion:
             seccion_id = entry["seccion_id"]
-            alumno_id = entry["alumno_id"]
+            alumno_import_id = entry["alumno_id"]
 
             try:
+                cursor.execute("""
+                    SELECT id FROM Users WHERE import_id = %s AND is_professor = FALSE
+                """, (alumno_import_id,))
+                user_row = cursor.fetchone()
+
+                if not user_row:
+                    self._error(f"No user found for alumno_import_id {alumno_import_id}")
+                    continue
+
+                alumno_id = user_row["id"]
+
                 cursor.execute("""
                     SELECT Instances.course_id
                     FROM Sections
@@ -241,7 +245,7 @@ class ImportService:
                 result = cursor.fetchone()
 
                 if not result:
-                    print(f"[ERROR] No course found for seccion_id {seccion_id}")
+                    self._error(f"No course found for seccion_id {seccion_id}")
                     continue
 
                 course_id = result['course_id']
@@ -250,23 +254,34 @@ class ImportService:
                     INSERT INTO Courses_Taken (user_id, section_id, course_id, final_grade)
                     VALUES (%s, %s, %s, %s)
                 """, (alumno_id, seccion_id, course_id, 0))
-                print(f"[SUCCESS] Enrolled alumno_id {alumno_id} in section_id {seccion_id} with course_id {course_id}")
+
+                self._success(f"Enrolled alumno_id {alumno_id} in section_id {seccion_id} (course_id {course_id})")
 
             except Exception as e:
-                print(f"[ERROR] Failed to enroll alumno_id {alumno_id} in section_id {seccion_id}: {e}")
-            
+                self._error(f"Failed to enroll alumno_import_id {alumno_import_id} in section_id {seccion_id}: {e}")
+
         self.db.commit()
 
     def _import_notas_alumnos(self, data):
         cursor = self.db.connect()
-        notas = data["notas"]
-        for entry in notas:
-            alumno_id = entry["alumno_id"]
+        for entry in data["notas"]:
+            alumno_import_id = entry["alumno_id"]
             topico_id = entry["topico_id"]
             instancia = entry["instancia"]
             nota = entry["nota"]
 
             try:
+                cursor.execute("""
+                    SELECT id FROM Users WHERE import_id = %s AND is_professor = FALSE
+                """, (alumno_import_id,))
+                user_row = cursor.fetchone()
+
+                if not user_row:
+                    self._error(f"No user found for alumno_import_id {alumno_import_id}")
+                    continue
+                
+                alumno_id = user_row["id"]
+                
                 cursor.execute("""
                     SELECT id FROM Activities
                     WHERE topic_id = %s AND instance = %s
@@ -274,7 +289,7 @@ class ImportService:
                 result = cursor.fetchone()
 
                 if not result:
-                    print(f"[ERROR] No activity found for topico_id {topico_id} and instancia {instancia}")
+                    self._error(f"No activity found for topico_id {topico_id} and instancia {instancia}")
                     continue
 
                 activity_id = result["id"]
@@ -283,12 +298,11 @@ class ImportService:
                     INSERT INTO Grades (activity_id, user_id, grade)
                     VALUES (%s, %s, %s)
                 """, (activity_id, alumno_id, nota))
-                print(f"[SUCCESS] Inserted grade for alumno_id {alumno_id}, activity_id {activity_id}: {nota}")
-
+                self._success(f"Inserted grade for alumno_id {alumno_id}, activity_id {activity_id}: {nota}")
             except Exception as e:
-                print(f"[ERROR] Failed to insert grade for alumno_id {alumno_id}, topico_id {topico_id}, instancia {instancia}: {e}")
+                self._error(f"Inserting grade for alumno_id {alumno_id}, topico_id {topico_id}, instancia {instancia}: {e}")
 
-            self.db.commit()
+        self.db.commit()
 
     def _import_salas_clases(self, data):
         cursor = self.db.connect()
@@ -305,10 +319,10 @@ class ImportService:
                     VALUES (%s, %s, %s)
                 """, (room_id, nombre, capacidad))
 
-                print(f"[SUCCESS] Room inserted: id={room_id}, name='{nombre}', capacidad={capacidad}")
+                self._success(f"Room inserted: id={room_id}, name='{nombre}', capacidad={capacidad}")
 
             except Exception as e:
-                print(f"[ERROR] Failed to insert room id={room_id}, name='{nombre}': {e}")
+                self._error(f"Failed to insert room id={room_id}, name='{nombre}': {e}")
             
         
         self.db.commit()
